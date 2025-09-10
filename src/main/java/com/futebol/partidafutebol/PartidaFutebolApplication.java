c9f73c690900957bc74469aee785a97cfb280d86
mmdomingues <matheusmdomingues55@gmail.com>
2025-09-10 09:58:20 -0300

    projeto configurado

---
+++ src/main/java/com/futebol/partidafutebol/PartidaFutebolApplication.java
@@ -0,0 +1,13 @@
+package com.futebol.partidafutebol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartidaFutebolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartidaFutebolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/futebol/partidafutebol/PartidoFutebolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futebol.partidafutebol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartidoFutebolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
